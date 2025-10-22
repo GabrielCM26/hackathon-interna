b4a338c2d4618e29f9c1dd47ff2de5a018f658ff
Miguel Sabogal <miguelsabogal20@gmail.com>
2025-10-23 00:02:41 +0100

    Create test.js

---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import AddTreeForm from "@/components/AddTreeForm";
+
+export default function TestPage() { 
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <AddTreeForm />
+    </div>
+  );
+}
