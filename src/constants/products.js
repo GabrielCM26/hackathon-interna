3bd951ebdad8686e15f76acbfb366e61a342db2b
Paula Guollo <paulaguollo00@gmail.com>
2025-10-23 03:49:29 +0100

    const-products

---
+++ src/constants/products.js
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+export const products = [
+  {
+    id: 1,
+    name: 'Sementes de Girassol',
+    category: 'seeds',
+    price: 25,
+    image: '/public/img/sementesdegirassol.jpg',
+    description: 'Pacote com 20 sementes para alegrar o teu jardim 🌻',
+  },
+  {
+    id: 2,
+    name: 'Kit de Compostagem',
+    category: 'tools',
+    price: 150,
+    image: '/public/img/compostagem.png',
+    description: 'Kit completo para compostagem caseira — transforma resíduos em vida!',
+  },
+  {
+    id: 3,
+    name: 'T-shirt Eco',
+    category: 'apparel',
+    price: 200,
+    image: '/public/img/camiseta.jpg',
+    description: '100% algodão orgânico, toque suave e consciente.',
+  },
+  {
+    id: 5,
+    name: 'Pá de Jardim',
+    category: 'tools',
+    price: 85,
+    image: 'public/img/pa.jpg',
+    description: 'Cabo de madeira sustentável e design ergonômico.',
+  },
+    ];
